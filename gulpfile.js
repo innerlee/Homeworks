828e74f83e2a1dbb88d7bf3b043dc9f8b011fce7
lizz <363664470@qq.com>
2017-02-23 09:01:54 +0800

    init co

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+"use strict";
+let gulp = require('gulp');
+let cp = require('child_process');
+let gutil = require('gulp-util');
+let fs = require("fs")
+
+// modify this to your main .tex file
+let f = 'bf';
+
+gulp.task('default', function() {
+  gutil.log('please wait...')
+  cp.exec('pdflatex -interaction=nonstopmode ' + f, (error, stdout, stderr) => {
+    if (error !== null) {
+      gutil.log(`exec error: ${error}`);
+      console.log(' ');
+      fs.readFile(f + ".log", "utf-8", (err, _data) => console.log(_data));
+      return;
+    }
+    gutil.log('bibtexing...');
+    cp.exec('bibtex ' + f + ' > bib.log', (error, stdout, stderr) => {
+      if (error !== null) {
+        gutil.log(`exec error: ${error}`);
+        console.log(' ');
+        fs.readFile("bib.log", "utf-8", (err, _data) => console.log(_data));
+        return;
+      }
+      gutil.log('pdflatexing...');
+      cp.exec('pdflatex -interaction=nonstopmode ' + f, (error, stdout, stderr) => {
+        if (error !== null) {
+          gutil.log(`exec error: ${error}`);
+          console.log(' ');
+          fs.readFile(f + ".log", "utf-8", (err, _data) => console.log(_data));
+          return;
+        }
+        gutil.log('pdflatexing...');
+        cp.exec('pdflatex -interaction=nonstopmode ' + f, (error, stdout, stderr) => {
+          if (error !== null) {
+            gutil.log(`exec error: ${error}`);
+            console.log(' ');
+            fs.readFile(f + ".log", "utf-8", (err, _data) => console.log(_data));
+            return;
+          }
+          cp.exec('start ' + f + '.pdf', (error, stdout, stderr) => {
+            gutil.log(`opening ${f}.pdf`);
+            gutil.log('love bufan.');
+          });
+        });
+      });
+    });
+  });
+});
